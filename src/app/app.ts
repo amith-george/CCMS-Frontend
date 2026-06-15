4343c571b23122aa89aa500c9c79ca0593ffa840
amith-george <amithgeorge130@gmail.com>
2026-06-15 12:29:27 +0530

    Frontend Initialization.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,18 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+import { Sidebar } from './components/sidebar/sidebar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Sidebar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('ccms-frontend');
+  private router = inject(Router);
+
+  get isLoginRoute(): boolean {
+    // Hide the sidebar if the URL contains '/login'
+    return this.router.url.includes('/login');
+  }
 }

7ca7ba5de82b76326a5f57a9fb00311f6be5b473
amith-george <amithgeorge130@gmail.com>
2026-06-15 17:38:09 +0530

    Login UI Created.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:5042/api' // Change this to your actual production URL when deployed
+  apiUrl: 'http://localhost:5042/api'
 };
